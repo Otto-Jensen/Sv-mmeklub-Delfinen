9849a2a08f7e9d66af182527df17a70c7f83f68f
Otto-Jensen1 <otje0002@stud.ek.dk>
2025-12-03 12:15:19 +0100

    orpet medlems funktion

--- src/Klub/Konkurrencesvoemmer.java
+++ src/Klub/Konkurrencesvoemmer.java
@@ -3,12 +3,16 @@ package Klub;
 import java.time.LocalDate;
 
 public class Konkurrencesvoemmer extends Medlem {
-    String traeningstid;
-    String konkurrencetid;
+    int traeningstid;
+    int konkurrencetid;
 
-    public Konkurrencesvoemmer(String navn, String adresse, LocalDate foedselsdag, int telefonNr, String traeningstid, String konkurrencetid) {
+    public Konkurrencesvoemmer(String navn, String adresse, LocalDate foedselsdag, int telefonNr, int traeningstid, int konkurrencetid) {
         super(navn, adresse, foedselsdag, telefonNr, "KonkurrenceSvømmer");
         this.traeningstid = traeningstid;
         this.konkurrencetid = konkurrencetid;
+    }
+
+    public Konkurrencesvoemmer(String navn, String adresse, LocalDate foedselsdag, int telefonNr){
+        super(navn, adresse, foedselsdag, telefonNr, "KonkurrenceSvømmer");
     }
 }
--- src/Klub/MedlemsListe.java
+++ src/Klub/MedlemsListe.java
@@ -2,8 +2,10 @@ package Klub;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MedlemsListe {
+    Scanner input = new Scanner(System.in);
     private ArrayList<Medlem> medlemmer=new ArrayList<>();
 
     public void tilfoejMedlem(Medlem x){
@@ -13,6 +15,48 @@ public class MedlemsListe {
     public ArrayList<Medlem> getMedlemmer() {
         return medlemmer;
     }
+
+  public void opretMedlem(){
+      System.out.println("Indstast kundens navn");
+      String name =input.nextLine();
+
+      System.out.println("Indtast fødselsdato");
+      String fd = input.nextLine();
+      LocalDate foedselsdag=LocalDate.parse(fd);
+
+      System.out.println("Indtast vejnavn");
+      String vejnavn = input.nextLine();
+      System.out.println("Indtast hus nummer");
+      int husNr=input.nextInt();
+      input.nextLine();//noget med at enter tæller som en \n, som bliver læst næste gang.
+      String adresse=vejnavn+String.valueOf(husNr);
+
+      System.out.println("Indtast telefon nummer");
+      int telefonNr=input.nextInt();
+
+      System.out.println("Indtast medlemstype");
+      String medlemstype=input.nextLine().toLowerCase();
+
+      Medlem medlem;
+
+      switch(medlemstype){
+          case "passiv":
+              medlem = new Passiv(name,adresse,foedselsdag,telefonNr);
+              break;
+          case "motionist":
+              medlem=new Motionist(name,adresse,foedselsdag,telefonNr);
+              break;
+          case "konkurrencesvømmer":
+              medlem=new Konkurrencesvoemmer(name,adresse,foedselsdag,telefonNr);
+              break;
+          default:
+              boolean Betalt=false;
+              System.out.println("Ukendt medlemtype - oprettes som standard medlem");
+              medlem = new Medlem(name,adresse,foedselsdag,telefonNr,medlemstype);
+      }
+      tilfoejMedlem(medlem);
+      System.out.println(medlem+" Er oprettet som medlem");
+  }
 
 
 //    public static void main(String[] args) {
